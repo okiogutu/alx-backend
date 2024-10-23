46a0e08f19ea91f372fe3bc804eebe87f6eeed3a
okiogutu <okiogutu@gmail.com>
2024-10-23 11:16:56 -0700

    review 19

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
-"""Bypass a locale on a page"""
-
-from flask import Flask, render_template, request
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
-class Config:
-    """Babel configutration"""
-    DEBUG = True
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
-
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Retrive locale for the web Page"""
-    locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
-        return locale
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
-    """Home Page"""
+    """
+    Handles / route
+    """
     return render_template('4-index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
